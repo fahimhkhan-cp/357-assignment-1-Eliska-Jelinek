456a6208282af0d0fe5cc4c6ca824f0e63b6e529
Eliška Jelinek <elcaj.sp@gmail.com>
2024-10-09 13:57:00 -0700

    first drafts of task1 and task2

--- unique.c
+++ unique.c
@@ -1,1 +1,34 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+int main(int argc, char *argv[]) {
+    // Open file
+    FILE *file = fopen(argv[1], "r");
+    // TODO: HANDLE NO FILE INPUT AND INVALID FILE INPUT
+
+    char *line = NULL;
+    unsigned int len = 0;
+    int read;
+    char *prev_line = NULL;
+
+    while ((read = getline(&line, (size_t *)&len, file)) != -1) {
+        // check if lines are the same
+        
+        int same = 0;
+        int ch1;
+        int ch2;
+        
+        if (!(prev_line != NULL && strcmp(prev_line, line) == 0)) {
+            printf("%s", line);
+        }
+        
+        // Update previous line with copy of current line
+        prev_line = strdup(line);
+    }
+
+    fclose(file);
+
+}
--- word_count.c
+++ word_count.c
@@ -1,5 +1,64 @@
-#include <stdin.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main() {
+// char read_cli_input(){
+//   return 'h';
+// }
+
+
+int main(int argc, char* argv[]) {
+	// Open input/source file for reading
+  FILE *sourcefile = stdin;
+  if (argc == 2) {
+    sourcefile = fopen(argv[1], "r");
+    // Error if it doesn't exist
+    if (sourcefile == NULL) {
+      perror("Source file does not exist");
+      return 1;
+    }
+  }
+
+
+	// Reading and writing from file
+
+  int chr_count = 0;
+  int word_count = 1; // might need to start this at one
+  int line_count = 1; // ^^
+
+	int ch; // char ch
+  int prev_space = 0; // false
+	while ((ch = getc(sourcefile)) != EOF) {
+    //putchar(ch);
+    
+    // Update character count
+    chr_count ++;
+
+    if (ch == *"\n") {
+      line_count += 1;
+      prev_space = 1;
+    } else if (isspace(ch) == 1) {
+      if (prev_space == 0) {  
+        word_count += 1;
+        prev_space = 1;
+      } else {
+        // reset prev_space
+        prev_space = 0;
+      }
+    } else {
+      prev_space = 0;
+    } 
+
+  }
+
+
+
+  printf("\nCharacter count: %d\nWord count: %d\nLine count: %d\n", chr_count,word_count,line_count);
+
+  // Close file
+  if (sourcefile != stdin) {
+    fclose(sourcefile);
+  }
+  
   return 0;
 }
