cbd5d4f3617406c72fb33278257fa930e8868532
Eliška Jelinek <elcaj.sp@gmail.com>
2024-11-14 14:36:18 -0800

    Updated word_count logic to properly count words

--- word_count.c
+++ word_count.c
@@ -21,22 +21,19 @@ int main(int argc, char* argv[]) {
 	while ((ch = getc(sourcefile)) != EOF) {
     // Update character count
     chr_count ++;
-
     // Update line count if applicable
     if (ch == *"\n") {
       line_count += 1;
       prev_space = 1; // sets to true
-    // Update if ch is a space (or other whitespace)
     } else if (isspace(ch) == 1) {
-      // Add to count only if previous char was not whitespace
-      if (prev_space == 0) {  
-        word_count += 1;
-        prev_space = 1;
-      } else {
-        // reset prev_space to false if it was true
-        prev_space = 0;
-      }
+      // Mark prev_space as whitespace
+      prev_space = 1;
     } else {
+      // If ch is the beginning of a new word (prev char was a space and current char is not)
+      if (prev_space == 1) {
+        // Update word count
+        word_count += 1;
+      }
       // reset prev_space to false
       prev_space = 0;
     } 
