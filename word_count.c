77ff8dfbc1c2bb3f69e2597479a9432cb29b9179
Eliška Jelinek <elcaj.sp@gmail.com>
2024-12-03 14:17:43 -0800

    fixed line number offset (1 --> 0)

--- word_count.c
+++ word_count.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
 	// Initialize counting variables
   int chr_count = 0;
   int word_count = 1; // Starts at 1 to include very first word
-  int line_count = 1; // ^^
+  int line_count = 0; // ^^
 
   // Initialize loop variables
 	int ch;
